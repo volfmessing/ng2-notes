4de8d6a0e9c7aafda410a6ecd1b8fca359ee9613
vit <vitalii.khodak@gmail.com>
2017-05-16 11:21:56 +0300

    add button to add note

--- app/notes.component.ts
+++ app/notes.component.ts
@@ -4,7 +4,31 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'notes',
-    template: `Notes list:`
+    template: `Notes list:
+    <ul>
+        <li *ngFor="let note of notes ">
+            {{note.text}}
+            <textarea [(ngModel)]="text"></textarea>
+            <button (click)="add()">Add</button>
+        </li>
+    </ul>`
 })
 export class NotesComponent {
+    notes: Note[] = [
+        {text: "Note one"},
+        {text: "Note two"}
+    ]
+
+    text: string
+
+    add() {
+        let note = {text: this.text}
+        this.notes.push(note);
+        this.text = "";
+    }
+}
+
+interface Note {
+    text: string;
+
 }
