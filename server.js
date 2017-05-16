291c06e3b4ad2fd5dbef781a40bb41e284b636d0
volfmessing <vitalii.khodak@gmail.com>
2017-05-16 15:10:34 +0300

    add session to express

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var session = require('express-session');
+
+var init_notes = [
+    {text: "First note"},
+    {text: "Second note"},
+    {text: "Third note"}
+];
+
+app.use(session({
+    secret: 'angular_tutorial',
+    resave: true,
+    saveUninitialized: true
+}));
+
 
 app.get("/notes", function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    var notes = [
-        {text: "First note"},
-        {text: "Second note"},
-        {text: "Third note"}
-    ]
-    res.send(notes);
+    if (!req.session.notes) {
+        req.session.notes = init_notes;
+    }
+    res.send(req.session.notes);
 });
 
 app.listen(8080);
