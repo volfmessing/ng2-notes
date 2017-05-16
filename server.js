344ac52441eb1fc48d6b58ce42d464aa038fbdd2
vit <vitalii.khodak@gmail.com>
2017-05-16 10:58:13 +0300

    basic skeleton

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.listen(8080);
+
+
+/*
+
+ FOLDER STRUCTURE:
+
+ root
+ app
+ server
+ server.js
+ package.json
+ index.html
+ package.json
+
+ */
+  
