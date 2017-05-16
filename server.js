57b3297f9535c3a737ea821edbdf1fa39d532129
volfmessing <vitalii.khodak@gmail.com>
2017-05-16 15:25:08 +0300

    add new note to session storage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var session = require('express-session');
 
-var init_notes = [
+var notes_initial = [
     {text: "First note"},
     {text: "Second note"},
     {text: "Third note"}
@@ -15,15 +15,24 @@ app.use(session({
     saveUninitialized: true
 }));
 
-
+//get default notes from session
 app.get("/notes", function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     if (!req.session.notes) {
-        req.session.notes = init_notes;
+        req.session.notes = notes_initial;
     }
     res.send(req.session.notes);
 });
+
+//add new note to session
+app.post("/notes", function (req, res) {
+    var note = req.body;
+    console.log("adding note", req.session.notes);
+    req.session.notes.push(note);
+    res.end();
+});
+
 
 app.listen(8080);
 
