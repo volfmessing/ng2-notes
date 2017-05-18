147be48bcb462eb5db63c06de194b9fc8ddbc88b
vit <vitalii.khodak@gmail.com>
2017-05-18 10:12:09 +0300

    added sections to app

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,7 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'my-app',
-    templateUrl: 'app.component.html',
-    template: `<h1>Notes Angular 2 App by {{name}}</h1>
-    <notes></notes>`
+    templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
     name = "Vit";
