17246b3d60eb3d4a71ef16a7f3efb5d7be94295c
vit <vitalii.khodak@gmail.com>
2017-05-18 11:57:20 +0300

    added add a  new section

--- app/sections.component.ts
+++ app/sections.component.ts
@@ -14,6 +14,8 @@ import {Http} from "@angular/http";
 export class SectionsComponent {
     private sectionsUrl = '/sections';
 
+    sectionsReplaceUrl = "/sections/replace";
+
     sections:Section[];
 
     activeSection:string = null;
@@ -45,10 +47,32 @@ export class SectionsComponent {
         this.sectionChanged.emit(this.activeSection);
         console.log("emmited section: ", section.title);
     }
+
+    addSection(newSection: HTMLInputElement) {
+        console.log("run add section for -", newSection.title);
+
+        let title = newSection.value;
+        if (!title) return;
+
+        // check for duplicates
+        if (this.sections.map(s => s.title).find(t => t === title)) return;
+
+        const section: Section = {title};
+        this.sections.unshift(section);
+        this.showSection(section);
+
+        // write sections to server and clear add section input box
+        this.writeSections().subscribe(res => newSection.value = "");
+    }
+
+    writeSections() {
+        return this.http.post(this.sectionsReplaceUrl, this.sections);
+    }
+
 }
 
-interface Section {
-    _id:string;
+export interface Section {
+    _id?: string;
     title:string;
 }
 
--- server/server.js
+++ server/server.js
@@ -101,6 +101,20 @@ app.get("/sections", function (req, res) {
     });
 });
 
+app.post("/sections/replace", function (req, resp) {
+    // do not clear the list
+    if (req.body.length == 0) {
+        resp.end();
+    }
+    db.sections.remove({}, function (err, res) {
+        if (err) console.log(err);
+        db.sections.insert(req.body, function (err, res) {
+            if (err) console.log("err after insert", err);
+            resp.end();
+        });
+    });
+});
+
 
 app.listen(8080);
 
