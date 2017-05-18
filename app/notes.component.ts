315724d9cf668faf73286daebecd9738ed112131
vit <vitalii.khodak@gmail.com>
2017-05-18 10:31:16 +0300

    added storing notes in mongoDB

--- app/notes.component.ts
+++ app/notes.component.ts
@@ -4,6 +4,7 @@
 import {Component} from "@angular/core";
 import {Http, RequestOptionsArgs, URLSearchParams} from "@angular/http";
 import "rxjs/add/operator/toPromise";
+import {Observable} from "rxjs";
 
 
 @Component({
@@ -15,6 +16,7 @@ export class NotesComponent {
 
     notes: Note[] = [];
     text: string;
+    section: string = 'Work';
 
     private notesUrl = 'http://localhost:8080/notes';  // URL to web api
 
@@ -23,14 +25,15 @@ export class NotesComponent {
     }
 
     loadNotes() {
-        this.getNotes().then(notes => {
+        this.getNotes();
+        /*.then(notes => {
             this.notes = notes
             console.log("notes are loaded", notes);
-        });
+         });*/
     }
 
     add() {
-        let note = {/*idx: null, */text: this.text}
+        let note = {text: this.text, section: this.section};
         this.addNote(note);
     }
 
@@ -53,10 +56,12 @@ export class NotesComponent {
 
     }
 
-    getNotes(): Promise<Note[]> {
-        return this.http.get(this.notesUrl, {withCredentials: true})
-            .toPromise()
-            .then(response => response.json() as Note[]);
+    getNotes(): Observable<Note[]> {
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('section', this.section);
+        let requestParams: RequestOptionsArgs = {search: params, withCredentials: true};
+        return this.http.get(this.notesUrl, requestParams)
+            .map(response => response.json() as Note[]);
     }
 
     addNote(note:Note) {
