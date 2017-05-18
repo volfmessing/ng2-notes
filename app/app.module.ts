fd2b66c84239fc6356fb92015dd0ae3aeadf5a91
vit <vitalii.khodak@gmail.com>
2017-05-18 14:27:03 +0300

    added section filtering with pipe

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,14 +5,18 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {DragulaModule} from "ng2-dragula";
+
+
 import {AppComponent} from "./app.component";
 import {NotesComponent} from "./notes.component";
 import {SectionsComponent} from "./sections.component";
-import {DragulaModule} from "ng2-dragula";
+import {SectionFilterPipe} from "./section.filter.pipe";
+
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpModule, DragulaModule],
-    declarations: [AppComponent, NotesComponent, SectionsComponent],
+    declarations: [AppComponent, NotesComponent, SectionsComponent, SectionFilterPipe],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ app/section.filter.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from "@angular/core";
+import {Section} from "./sections.component";
+/**
+ * Created by dp-ptcstd-32 on 5/18/2017.
+ */
+@Pipe({
+    name: 'sectionFilter'
+})
+export class SectionFilterPipe implements PipeTransform {
+    transform(sections: Section[], v: string): Section[] {
+        if (!sections) return [];
+        return sections.filter(
+            s => s.title.toLowerCase().startsWith(v.toLowerCase()));
+    }
+}
