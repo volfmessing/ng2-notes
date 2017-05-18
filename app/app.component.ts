235a3c12f70cd4e413d44c6730e40c7623af70ae
volfmessing <vitalii.khodak@gmail.com>
2017-05-18 16:37:11 +0300

    add routing part2

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,15 +2,12 @@
  * Created by dp-ptcstd-32 on 5/15/2017.
  */
 import {Component} from "@angular/core";
+
+
 @Component({
     selector: 'my-app',
     templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
-    name = "Vit";
-    section: string;
-
-    setSection(section: string) {
-        this.section = section;
-    }
+    name = "Vit";    
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -6,17 +6,22 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {DragulaModule} from "ng2-dragula";
-
-
+import {RouterModule, Routes} from "@angular/router";
 import {AppComponent} from "./app.component";
 import {NotesComponent} from "./notes.component";
 import {SectionsComponent} from "./sections.component";
 import {SectionFilterPipe} from "./section.filter.pipe";
+import {PageNotFound} from "./page.not.found.component";
+import {NotesEditorComponent} from "./notes.editor.component";
 
+const appRoutes:Routes = [
+    {path: '', component: NotesEditorComponent},
+    {path: '**', component: PageNotFound}
+];
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpModule, DragulaModule],
-    declarations: [AppComponent, NotesComponent, SectionsComponent, SectionFilterPipe],
+    imports: [BrowserModule, FormsModule, HttpModule, DragulaModule, BrowserModule, RouterModule.forRoot(appRoutes)],
+    declarations: [AppComponent, NotesComponent, SectionsComponent, SectionFilterPipe, PageNotFound, NotesEditorComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
